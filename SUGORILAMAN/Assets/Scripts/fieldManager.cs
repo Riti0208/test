b2070a6f71fb52b71f14762338ca82cfee12ab58
くらくら <daichizaki@yahoo.co.jp>
2017-02-25 13:01:46 +0900

    1zi
    １時です！！

---
+++ SUGORILAMAN/Assets/Scripts/fieldManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fieldManager : MonoBehaviour {
+
+	public GameObject hardBlock;
+	public int[,] fieldIndex;
+
+	// Use this for initialization
+	void Start () {
+		fieldIndex = new int[17, 13];
+		for (int i = 0; i < 17; i++) {
+			Instantiate(hardBlock, new Vector3(i * 0.4f - 3.2f, 4f, 0f), Quaternion.identity);
+		}
+		for (int i = 0; i < 11; i++)
+		{
+			Instantiate(hardBlock, new Vector3(-3.2f, 3.6f - i * 0.4f, 0f), Quaternion.identity);
+		}
+		for (int i = 0; i < 17; i++)
+		{
+			Instantiate(hardBlock, new Vector3(i * 0.4f - 3.2f, -0.8f, 0f), Quaternion.identity);
+		}
+		for (int i = 0; i < 11; i++)
+		{
+			Instantiate(hardBlock, new Vector3(3.2f, 3.6f - i * 0.4f, 0f), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SUGORILAMAN/Assets/Scripts/player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class player : MonoBehaviour {
+
+
+	private int posX;
+	private int posY;
+	private bool isMove;
+	Vector2 rot;
+	bool push;
+	public void PushDown()
+	{
+		push = true;
+	}
+	public void PushUp()
+	{
+		push = false;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (push) {
+			transform.Translate(Time.deltaTime * rot);
+		}
+	}
+	public void moveUp() {
+		rot = new Vector2(0f, 1f);
+	}
+	public void moveDown() {
+		rot = new Vector2(0f, -1f);
+	}
+	public void moveRight() {
+		rot = new Vector2(1f, 0f);
+	}
+	public void moveLeft() {
+		rot = new Vector2(-1f, 0f);
+	}
+}
